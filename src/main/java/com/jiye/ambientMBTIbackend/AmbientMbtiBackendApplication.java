141979986eac4329117b48001ab37f530e4d9f7c
jiyeeeah <sije3757@snu.ac.kr>
2023-12-11 21:56:31 +0900

    Chore: 초기 세팅

---
+++ src/main/java/com/jiye/ambientMBTIbackend/AmbientMbtiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jiye.ambientMBTIbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbientMbtiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbientMbtiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiye/ambientMBTIbackend/AmbientMbtiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiye.ambientMBTIbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbientMbtiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
